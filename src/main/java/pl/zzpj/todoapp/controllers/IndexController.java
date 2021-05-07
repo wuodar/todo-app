6724340c55bb6b3fa4464a9e97654a3195eb17dd
Artur <artur300998@gmail.com>
2021-05-07 21:58:10 +0200

    Added Travis Integration

--- src/main/java/pl/zzpj/todoapp/controllers/IndexController.java
+++ src/main/java/pl/zzpj/todoapp/controllers/IndexController.java
@@ -1,15 +1,21 @@
 package pl.zzpj.todoapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/helloWorld")
-    public String helloWorld() {
+    @GetMapping("/helloWorld/{name}")
+    public String helloWorld(@PathVariable String name, Model model, HttpServletRequest request) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
