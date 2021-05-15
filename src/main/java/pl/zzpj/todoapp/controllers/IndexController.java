f20da951a0e881296f3edbb161f02f98eeac810e
dmorka <morkadawid@gmail.com>
2021-05-15 11:24:52 +0200

    designed index page

--- src/main/java/pl/zzpj/todoapp/controllers/IndexController.java
+++ src/main/java/pl/zzpj/todoapp/controllers/IndexController.java
@@ -3,19 +3,14 @@ package pl.zzpj.todoapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/helloWorld/{name}")
-    public String helloWorld(@PathVariable String name, Model model, HttpServletRequest request) {
-        model.addAttribute("name", name);
+    @GetMapping("/")
+    public String helloWorld(Model model, HttpServletRequest request) {
         return "index";
     }
 }
